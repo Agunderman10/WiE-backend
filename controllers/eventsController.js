2e1a294278ec8dc21ba90117116f50c4bdaded13
agunderman10 <agunderman10@gmail.com>
2020-09-10 09:40:07 -0400

    Added new data to events

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,3 +1,58 @@
-exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: eventsController.index');
-}
+exports.index = function (req, res) {
+  res.json({
+    osu_events: [
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Empowherment Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Health Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Session",
+      },
+    ],
+    doi_events: [
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Empowherment Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Health Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Session",
+      },
+    ],
+    wie_lc_events: [
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Empowherment Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Health Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Mental Session",
+      },
+      {
+        image: "./../../../images/Oval.jpg",
+        label: "WiE Session",
+      },
+    ],
+  });
+};
