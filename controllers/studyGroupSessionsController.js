d9f5f22101a53668bbf3f014b73257c012089e5b
agunderman10 <agunderman10@gmail.com>
2020-09-28 11:11:02 -0400

    Added server-database connection for study groups

--- controllers/studyGroupSessionsController.js
+++ controllers/studyGroupSessionsController.js
@@ -1,3 +1,24 @@
-exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: studyGroupSessionsController.index');
-}
+const dbh = require("../database/index");
+
+exports.index = function (req, res) {
+  dbh.getConnection((error, connection) => {
+    if (error) {
+      connection.release();
+      console.log(error);
+      res.send("error in connection");
+    } else {
+      const SELECT_LABELS_SQL = "SELECT label, time, class FROM study_groups;";
+
+      dbh.query(SELECT_LABELS_SQL, (error, results) => {
+        if (error) {
+          connection.release();
+          console.log(error);
+          res.send("error in query");
+        } else {
+          connection.release();
+          res.send(results);
+        }
+      });
+    }
+  });
+};
