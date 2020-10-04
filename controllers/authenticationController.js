5c694e360c1bcf8d446d5a7e44b0f551eb5ff36e
agunderman10 <agunderman10@gmail.com>
2020-10-03 22:38:03 -0400

    Added mysql checks of email and password. will add encryption soon

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -24,5 +24,30 @@ exports.index = function (req, res) {
 };
 
 exports.sign_in_post = function (req, res) {
-  res.send({email: req.body.email, password: req.body.password });
+  var email = req.body.email;
+  var password = req.body.password;
+
+  dbh.getConnection((error, connection) => {
+    if (error) {
+      connection.release();
+      console.log(error);
+      res.send("error in connection");
+    } else {
+      dbh.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, password], (error, row, field) => {
+        if (error) {
+          connection.release();
+          console.log(error);
+          res.send({ success: false });
+        }
+        
+        if(row.length > 0) {
+          connection.release();
+          res.send({ success: true });
+        }
+        else {
+          res.send({ success: false });
+        }
+      });
+    }
+  });
 };
