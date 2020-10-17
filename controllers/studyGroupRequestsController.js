925e99f3b5a232cebed756e9fce188718a971351
agunderman10 <agunderman10@gmail.com>
2020-10-17 01:06:21 -0400

    Added get request for requests

--- controllers/studyGroupRequestsController.js
+++ controllers/studyGroupRequestsController.js
@@ -1,47 +1,67 @@
 const dbh = require("../database/index");
 
 exports.get_study_group_requests = function (req, res) {
-    res.send('NOT IMPLEMENTED');
-}
+  dbh.getConnection((error, connection) => {
+    if (error) {
+      connection.release();
+      console.log(error);
+      res.send("error in connection");
+    } else {
+      const SELECT_LABELS_SQL =
+        "SELECT label, link, date, time, timeIsAmOrPm, class FROM requests LIMIT 10;";
 
-exports.post_study_group_request = function (req, res) {
-    dbh.getConnection((error, connection) => {
+      dbh.query(SELECT_LABELS_SQL, (error, results) => {
         if (error) {
           connection.release();
           console.log(error);
-          res.send("error in connection");
+          res.send("error in query");
         } else {
-          const label = req.body.name;
-          const link = req.body.link;
-          const date = req.body.date;
-          const time = req.body.time;
-          const timeIsAmOrPm = req.body.timeIsAmOrPm;
-          const type = req.body.selectedCategory;
-          const SELECT_LABELS_SQL =
-            "INSERT INTO requests(label, link, date, time, timeIsAmOrPm, class) VALUES(\'" +
-            label +
-            "\',\'" +
-            link +
-            "\',\'" +
-            date +
-            "\',\'" +
-            time +
-            "\',\'" +
-            timeIsAmOrPm +
-            "\',\'" +
-            type +
-            "\');";
-    
-          dbh.query(SELECT_LABELS_SQL, (error, results) => {
-            if (error) {
-              connection.release();
-              console.log(error);
-              res.send("error in query");
-            } else {
-              connection.release();
-              res.send(results);
-            }
-          });
+          connection.release();
+          res.send(results);
         }
       });
-}
+    }
+  });
+};
+
+exports.post_study_group_request = function (req, res) {
+  dbh.getConnection((error, connection) => {
+    if (error) {
+      connection.release();
+      console.log(error);
+      res.send("error in connection");
+    } else {
+      const label = req.body.name;
+      const link = req.body.link;
+      const date = req.body.date;
+      const time = req.body.time;
+      const timeIsAmOrPm = req.body.timeIsAmOrPm;
+      const type = req.body.selectedCategory;
+      const SELECT_LABELS_SQL =
+        "INSERT INTO requests(label, link, date, time, timeIsAmOrPm, class) VALUES('" +
+        label +
+        "','" +
+        link +
+        "','" +
+        date +
+        "','" +
+        time +
+        "','" +
+        timeIsAmOrPm +
+        "','" +
+        type +
+        "');";
+
+      dbh.query(SELECT_LABELS_SQL, (error, results) => {
+        if (error) {
+          connection.release();
+          console.log(error);
+          res.send("error in query");
+        } else {
+          connection.release();
+          res.send(results);
+        }
+      });
+    }
+  });
+};
