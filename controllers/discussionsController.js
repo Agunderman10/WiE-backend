99857d38c578c05e2b2ff717e2bacddbe8c80bdb
agunderman10 <agunderman10@gmail.com>
2020-12-15 10:33:40 -0500

    Added initial code for getting discussion data

--- controllers/discussionsController.js
+++ controllers/discussionsController.js
@@ -1,3 +1,23 @@
-exports.getDiscussions = function(req, res) {
-    res.send('NOT IMPLEMENTED: discussionsController.index');
-}
+exports.getDiscussions = function (req, res) {
+  dbh.getConnection((error, connection) => {
+    if (error) {
+      connection.release();
+      console.log(error);
+      res.send("error in connection");
+    } else {
+      const SELECT_LABELS_SQL =
+        "SELECT discussion_id, poster_name, message, timestamp FROM discussions LIMIT 10;";
+
+      dbh.query(SELECT_LABELS_SQL, (error, results) => {
+        if (error) {
+          connection.release();
+          console.log(error);
+          res.send("error in query");
+        } else {
+          connection.release();
+          res.send(results);
+        }
+      });
+    }
+  });
+};
